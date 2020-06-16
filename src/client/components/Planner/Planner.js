513591248e32b6477ae8d9dedf60799081a8fe62
mattosmia <mattosmia@gmail.com>
2020-06-17 00:55:48 +0100

    Added datepicker to planner

--- src/client/components/Planner/Planner.js
+++ src/client/components/Planner/Planner.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from "react-datepicker";
+ 
+import './DatePicker.scss';
 import './Planner.scss';
 
 export default function Planner() {
+  const date = new Date();
+  
+  const [startDate, setStartDate] = useState(date);
+  
+  const handleDateChange = date => {
+    setStartDate(date);
+  };
+
   return (
     <section className="planner">
       <h1>Planner</h1>
+      <DatePicker
+        dateFormat="dd/MM/yyyy"
+        selected={startDate}
+        onChange={handleDateChange}
+      />
+      <div className="planner__wrapper">
+        
+      </div>
     </section>
   )
 }
