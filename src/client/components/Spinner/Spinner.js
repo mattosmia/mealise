6a05c551c0484062ebc60c5c7d610e62ff928ea1
mattosmia <mattosmia@gmail.com>
2020-06-19 00:16:38 +0100

    Added spinner (loader)

---
+++ src/client/components/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Spinner.scss';
+
+export default function Spinner() {
+  return (
+      <div className="spinner">
+          <div className="spinner__image"></div>
+      </div>
+  )
+}
