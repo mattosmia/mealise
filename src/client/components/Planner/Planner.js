00be0230caec5802c817bb10f9b90c84f97b4851
mattosmia <mattosmia@gmail.com>
2020-06-19 00:17:33 +0100

    Tweaks to Planner

--- src/client/components/Planner/Planner.js
+++ src/client/components/Planner/Planner.js
@@ -3,6 +3,7 @@ import DatePicker from "react-datepicker";
  
 import './Planner.scss';
 import PlannerDay from './PlannerDay';
+import Spinner from '../Spinner/Spinner';
 
 export default function Planner() {
   const initialEndDate = new Date();
@@ -11,6 +12,7 @@ export default function Planner() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(initialEndDate);
   const [plannerRange, setPlannerRange] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   
   useEffect(() => {
     const newDateRange = [];
@@ -22,8 +24,10 @@ export default function Planner() {
 
   return (
     <section className="planner">
+      { isLoading && <Spinner /> }
       <h1>Planner</h1>
       <div className="planner__datepicker">
+        <label><span>From:</span>
         <DatePicker
           selected={startDate}
           onChange={date => setStartDate(date)}
@@ -33,7 +37,8 @@ export default function Planner() {
           maxDate={endDate}
           todayButton="Today"
           dateFormat="dd/MM/yyyy"
-        />
+        /></label>
+        <label><span>To:</span>
         <DatePicker
           selected={endDate}
           onChange={date => setEndDate(date)}
@@ -43,7 +48,7 @@ export default function Planner() {
           minDate={startDate}
           todayButton="Today"
           dateFormat="dd/MM/yyyy"
-        />
+        /></label>
       </div>
       <div className="planner__wrapper">
         { !plannerRange.length?
--- src/client/components/Planner/PlannerDay.js
+++ src/client/components/Planner/PlannerDay.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default function PlannerDay({ date }) {
+  const handleAddMeal = () => {
+    console.log('add meal')
+  }
+
   return (
     <div className="planner__wrapper__card">
       <div className="planner__wrapper__card__heading">
@@ -15,6 +19,9 @@ export default function PlannerDay({ date }) {
         </div>
         <div className="planner__wrapper__card__meal" style={{backgroundColor: 'orange'}}>
           Ham & cheese sandwich
+        </div>
+        <div className="planner__wrapper__card__meal planner__wrapper__card__meal--add" style={{backgroundColor: 'skyblue'}}>
+          <button onClick={handleAddMeal}><span className="vh">Plan meal</span></button>
         </div>
       {/* </div> */}
     </div>
