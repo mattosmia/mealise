93ce8a2e050ad827e98ab98b5ba368a1bf547481
mattosmia <mattosmia@gmail.com>
2020-06-20 23:40:44 +0100

    Added react-axe for a11y checks

--- src/client/index.js
+++ src/client/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('app')
-);
+if (process.env.NODE_ENV !== 'production') {
+  import('react-axe').then(axe => {
+    axe.default(React, ReactDOM, 1000);
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById('app')
+    )
+  });
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('app')
+  )
+}
 
 serviceWorker.unregister();
