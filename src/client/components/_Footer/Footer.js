144d121123786fe3c24609a946e2953684fab1e2
mattosmia <mattosmia@gmail.com>
2020-06-20 23:42:34 +0100

    Removed nav (duplicate landmark issue - a11y)

--- src/client/components/_Footer/Footer.js
+++ src/client/components/_Footer/Footer.js
@@ -6,15 +6,13 @@ import './Footer.scss';
 export default function Footer() {
   return (
     <footer>
-      <nav>
-        <ul>
-          <li><Link to="/about-us">About us</Link></li>
-          <li><Link to="/terms-conditions">Terms & conditions</Link></li>
-          <li><Link to="/privacy-policy">Privacy policy</Link></li>
-          <li><Link to="/cookie-policy">Cookie policy</Link></li>
-          <li><Link to="/contact-us">Contact us</Link></li>
-        </ul> 
-      </nav>
+      <ul>
+        <li><Link to="/about-us">About us</Link></li>
+        <li><Link to="/terms-conditions">Terms & conditions</Link></li>
+        <li><Link to="/privacy-policy">Privacy policy</Link></li>
+        <li><Link to="/cookie-policy">Cookie policy</Link></li>
+        <li><Link to="/contact-us">Contact us</Link></li>
+      </ul> 
     </footer>
   )
 }
