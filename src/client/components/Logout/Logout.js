3fbff7cadcad05eef52e4852de3af912d6f8c13b
mattosmia <mattosmia@gmail.com>
2020-07-18 00:34:47 +0100

    Simplifying logout

--- src/client/components/Logout/Logout.js
+++ src/client/components/Logout/Logout.js
@@ -1,43 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import axios from "axios";
+import React, { useEffect } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 import cookie from 'react-cookies';
 
-import { authHeaders } from '../../helpers/auth';
 import './Logout.scss';
 
 import { jwtCookieName } from '../../helpers/cookies';
-import PageContext from '../../helpers/pageContext';
 
 export default function Logout() {
-  const page = useContext(PageContext);
-  const [requestStatus, setRequestStatus] = useState('');
-
   const history = useHistory();
   
-  page.setIsLoading(true);
-  
   useEffect(() => {
-    axios.post('/api/user/logout', {}, authHeaders())
-      .then(res =>
-        setRequestStatus('success')
-      ).catch(err => 
-        setRequestStatus('error')
-      ).finally(() =>
-        page.setIsLoading(false)
-      );
-  }, [])
-
-  useEffect(() => {
-    if (requestStatus === 'success') {
       cookie.remove(jwtCookieName, { path: '/' })
       history.push({
         pathname:  "/"
       })
-    }
-  }, [requestStatus])
+  }, [])
 
   return (
-    <>LOGGED OUT</>
+    <p>We are logging you out... Click <Link to={'/'}>here</Link> if you're not redirected automatically.</p>
   )
 }
