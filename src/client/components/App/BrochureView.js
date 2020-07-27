0091f18b0dfb51898218f6d18697943b39a5c850
mattosmia <mattosmia@gmail.com>
2020-07-27 18:00:27 +0100

    Added user check to BrochureView

--- src/client/components/App/BrochureView.js
+++ src/client/components/App/BrochureView.js
@@ -2,12 +2,24 @@ import React, { useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../_Footer/Footer';
 import Header from '../_Header/BrochureHeader';
+import { isAuth } from '../../helpers/auth';
+import UserContext from '../../helpers/userContext';
 import PageContext from '../../helpers/pageContext';
 
 export default function BrochureView({ children }) {
   const page = useContext(PageContext);
+  const user = useContext(UserContext);
 
-  useEffect(() => page.setIsLoading(false), []);
+  useEffect(() => {
+    isAuth()
+      .then(res => {
+        if (res.data) {
+          user.setUser(res.data);
+        }
+      }).catch(err => 
+        console.log(err)     
+      ).finally(() => page.setIsLoading(false))
+  }, []);
 
   return (
     <>
