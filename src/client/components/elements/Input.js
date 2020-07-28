68dc53b7fcdae6f5b5dc803eb79dc99c6d882c0d
mattosmia <mattosmia@gmail.com>
2020-07-28 10:43:42 +0100

    Added reveal password functionality

--- src/client/components/elements/Input.js
+++ src/client/components/elements/Input.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 export default function Input({ label, type, name, value, isRequired, errorMsg, handleChange, handleBlur }) {
+  const inputField = useRef(null);
+  const togglePassword = () => {
+    inputField.current.type = inputField.current.type === 'password'? 'text' : 'password'
+  }
+
   return (
     <label>
       <span className={(isRequired && 'label--required') || null}>{label}</span>
-      <input type={type || `text`} name={name} value={value} onChange={handleChange} onBlur={handleBlur} className={errorMsg && 'input--error'} />
+      <div className="input__wrapper">
+        <input type={type || `text`} name={name} value={value} onChange={handleChange} onBlur={handleBlur} ref={inputField} className={errorMsg && 'input--error'} />
+        { type === 'password' && <Button classes="button--icon input__password-toggle" handleClick={togglePassword}><span className="vh">Toggle view password</span></Button> }
+      </div>
       <div aria-live="assertive">
         { errorMsg && 
           <span className="input__error-message">{ errorMsg }</span>
