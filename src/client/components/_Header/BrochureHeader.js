233e4863ec60a86cdd323619e38a49368086c37d
mattosmia <mattosmia@gmail.com>
2020-07-28 18:37:46 +0100

    Added logo link to logo (brochure view)

--- src/client/components/_Header/BrochureHeader.js
+++ src/client/components/_Header/BrochureHeader.js
@@ -9,21 +9,23 @@ export default function Header() {
   const user = useContext(UserContext);
   return (
     <header>
-        <h1 className="header__logo">
-            <span className="vh">Mealise</span>
-        </h1>
-        <nav id="main-nav">
-            <ul>
-          { user.user && user.user.id ?
-              <li><Link to={"/planner"} className="nav__cta nav__cta--secondary">Go back to planner &rarr;</Link></li>
-            :
-            <>
-              <li><Link to="/login" className="nav__cta nav__cta--secondary">Log in</Link></li>
-              <li><Link to="/register" className="nav__cta nav__cta--primary">Sign up</Link></li>
-            </>
-          }
-            </ul>
-        </nav>
+      <h1 className="header__logo">
+        <Link to={'/'} className="header__logo__link">
+          <span className="vh">Mealise</span>
+        </Link>
+      </h1>
+      <nav id="main-nav">
+        <ul>
+        { user.user && user.user.id ?
+          <li><Link to={"/planner"} className="nav__cta nav__cta--secondary">Go back to planner &rarr;</Link></li>
+          :
+          <>
+          <li><Link to="/login" className="nav__cta nav__cta--secondary">Log in</Link></li>
+          <li><Link to="/register" className="nav__cta nav__cta--primary">Sign up</Link></li>
+          </>
+        }
+        </ul>
+      </nav>
     </header>
   )
 };
